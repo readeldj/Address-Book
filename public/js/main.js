67b4e564745205c26eefe2f5c8640b3eec6e0304
Devin Readel <readeldj@gmail.com>
2015-02-11 14:06:14 -0600

    just before breaking it

--- public/js/main.js
+++ public/js/main.js
@@ -2,36 +2,65 @@
 
 'use strict';
 
-//var $form        = $('form'),
-//    $tbody       = $('tbody'),
-//    FIREBASE_URL = 'https://djr-address-book.firebaseio.com/',
-//    fb           = new Firebase(FIREBASE_URL),
+var $form        = $('form'),
+    $tbody       = $('tbody'),
+    FIREBASE_URL = 'https://djr-address-book.firebaseio.com/';
+//    fb           = new Firebase(FIREBASE_URL)
 //    usersFbUrl   = FIREBASE_URL + '/users/' + fb.getAuth().uid + '/data';
 
+$.get(FIREBASE_URL + '/users.json', function (res) {
+  Object.keys(res).forEach(function (uuid) {
+    addRowToTable(uuid, res[uuid]);
+  });
+});
 
-
+function addRowToTable(uuid, data){
+  var $tr = $('<tr><td><img src="' + data.photo + '"class=pic></td><td>' + data.name + '</td><td>' + data.phone + '</td><td>' + data.email + '</td><td>' + data.twitter + '</td></tr>');
+  $tr.attr('data-uuid', uuid);
+  $tbody.append($tr);
+}
 
 var $select = $('.newContact')
 $select.click(function() {
   $('.hidden').show('slow');
 });
 
-function createTR(obj) {
-  var $tr = $('<tr></tr>');
+$form.submit(function(evt){
+  var $photo   = $('input[name="photo"]').val(),
+      $name    = $('input[name="name"]').val(),
+      $phone   = $('input[name="phone"]').val(),
+      $email   = $('input[name="email"]').val(),
+      $twitter = $('input[name="twitter"]').val(),
+      $tbody   = $('tbody'),
+      $tr      = $('<tr><td><img src="' + $photo + '"class=pic></td><td>' + $name + '</td><td>' + $phone + '</td><td>' + $email + '</td><td>' + $twitter + '</td></tr>');
 
-  $tr.append('<td class="photo">' + obj.Name + '</td>');
-  $tr.append('<td class="name">' + obj.LastPrice + '</td>');
-  $tr.append('<td class="phone">' + $('#quantity').val() + '</td>');
-  $tr.append('<td class="email">' + obj.Change.toFixed(4) + '</td>');
-  $tr.append('<td class="twitter">' + obj.ChangePercent.toFixed(4) + '</td>');
-  $tr.append('<td><button id="remove">Remove</td>');
+  var group = { photo: $photo, name: $name, phone: $phone, email: $email, twitter: $twitter };
+  var url  = 'https://djr-address-book.firebaseio.com/users.json';
+  var data = JSON.stringify(group);
+  $.post(url, data, function(res){
+    $tr.attr('data-uuid', res.name);
+    $tbody.append($tr);
+  });
 
-  return $tr;
-}
-$removeButton = $('button#remove');
-$removeButton.on('click', removeTR).on('click', updateTotal, updateValue);
+  clearForm();
 
-function removeTR() {
-  $(this).parent().parent().remove();
-}
+  evt.preventDefault();
+});
+
+function clearForm(){
+  var $photo = $('input[name="photo"]'),
+      $name = $('input[name="name"]'),
+      $phone = $('input[name="phone"]'),
+      $email = $('input[name="email"]'),
+      $twitter = $('input[name="twitter"]');
+  $photo.val('')
+  $name.val('')
+  $phone.val('')
+  $email.val('')
+  $twitter.val('')
+  $('.hidden').hide('slow');
+
+};
+
+
 
